c42d6bef907a9437eb54c4891b9a204b4a34bd90
Akash <emailforwork007@gmail.com>
2021-03-09 18:15:41 +0530

    commit changes

---
+++ Final Case Study_MyFoodSupply/FoodSupply.UnitTest/FoodSupply.UnitTest.cs
@@ -0,0 +1,124 @@
+﻿using MyFoodSupply;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodSupply.UnitTest
+{
+    public class FoodSupply
+    {
+        private Program program;
+
+        [SetUp]
+        public void SetUp()
+        {
+            program = new Program();
+        }
+
+        // NUnit test cases for Food detail
+        [Test]
+        [TestCase("abc", 2, "2021-04-08", 100.0)]
+        public void FoodDetailTest(string name, int dishType, DateTime expiryDate, double price)
+        {
+            /*  var foodItem = new FoodDetail()
+
+              {
+
+                  Name = name,
+
+                  ExpiryDate = expiryDate,
+
+                  Price = price,
+
+                  DishType = (FoodDetail.Category)dishType
+
+              };*/
+            var result = program.CreateFoodDetail(name, dishType, expiryDate, price);
+
+            Assert.That(result, Is.TypeOf<FoodDetail>());
+        }
+        [Test]
+        [TestCase(null, 1, "2021-04-08", 256.56)]
+        [TestCase("", 2, "2021-04-08", 0.01)]
+        public void FoodNameisEmpty(string name, int dishType, DateTime expiryDate, double price)
+        {
+            Assert.Throws<Exception>(() => program.CreateFoodDetail(name, dishType, expiryDate, price));
+        }
+
+        [Test]
+        [TestCase("abc", 3, "2021-04-08", 0)]
+        [TestCase("abc", 2, "2021-04-08", -1)]
+        public void FoodPriceisLessThanZero(string name, int dishType, DateTime expiryDate, double price)
+        {
+            Assert.Throws<Exception>(() => program.CreateFoodDetail(name, dishType, expiryDate, price));
+        }
+
+        [Test]
+        [TestCase("abc", 1, "2020-04-08", 1)]
+        public void ImprobableExpiryDate(string name, int dishType, DateTime expiryDate, double price)
+        {
+            Assert.Throws<Exception>(() => program.CreateFoodDetail(name, dishType, expiryDate, price));
+        }
+
+
+        [Test]
+        [TestCase(1, "2021-04-08", "abc", 100)]
+        public void SupplyDetailObjectCreatedTest(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            var foodDetail = new FoodDetail()
+            {
+                Name = "abc",
+                DishType = (FoodDetail.Category)1,
+                Price = 150,
+                ExpiryDate = new DateTime(2021, 4, 10),
+            };
+
+            var result = program.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, foodDetail);
+
+            Assert.That(result, Is.TypeOf<SupplyDetail>());
+        }
+
+
+        [Test]
+        [TestCase(-1, "2021-04-08", "abc", 100)]
+        [TestCase(0, "2021-04-08", "abc", 100)]
+        public void FoodCountLessThanEqualToZero(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+         {
+            var foodDetail = new FoodDetail()
+            {
+                Name = "abc",
+                DishType = (FoodDetail.Category)1,
+                Price = 150,
+                ExpiryDate = new DateTime(2021, 4, 10),
+            };
+            Assert.Throws<Exception>(() => program.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, foodDetail));
+        }
+
+
+        [Test]
+        [TestCase(1, "2021-01-08", "abc", 100)]
+        public void RequestDateLessThanCurrentDateTest(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            var foodDetail = new FoodDetail()
+            {
+                Name = "abc",
+                DishType = (FoodDetail.Category)1,
+                Price = 150,
+                ExpiryDate = new DateTime(2021, 4, 10),
+            };
+            Assert.Throws<Exception>(() => program.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, foodDetail));
+        }
+
+
+        [TestCase(1, "2021-04-08", "abc", 100,null)]
+        public void NullFoodObjectTest(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge,FoodDetail foodDetail)
+        {
+            var result = program.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, foodDetail);
+            Assert.That(result, Is.EqualTo(null));
+        }
+
+    }
+}
---
+++ Mock Case Study_Generic Medicine/GenericMedicineTest.UnitTest/GenericMedicineTest.cs
@@ -0,0 +1,125 @@
+﻿using GenericMedicine;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMedicineTest.UnitTest
+{
+    [TestFixture]
+   public class GenericMedicineTest
+    {
+        private Program program;
+
+        [SetUp]
+        public void SetUp()
+        {
+            program = new Program();
+        }
+        [Test]
+       
+        public void CreateMedicineObjectTest()
+        {
+            Medicine medicine = new Medicine()
+            {
+                Id = 1,
+                Name = "montelukast",
+                GenericMedicineName = "xyz",
+                Composition = "mno",
+                ExpiryDate = new DateTime(2021, 5, 15),
+                PricePerStrip = 200
+            };
+           
+            var result = program.CreateMedicineDetail(medicine.Name, medicine.GenericMedicineName, medicine.Composition, medicine.ExpiryDate, medicine.PricePerStrip);
+            Assert.That(result, Is.TypeOf<Medicine>());
+        }
+   [Test]
+ [TestCase("cetrizine", null, "antihistamine", "2021-03-15", 100)]
+ [TestCase("cetrizine", "", "antihistamine", "2021-03-15", 100)]
+ public void ExceptionMedicineNameTest(string name, string genericMedicineName, string composition, DateTime expiryDate, double pricePerStrip)
+ {
+     Assert.Throws<Exception>(() => program.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, pricePerStrip));
+ }
+
+ [Test]
+ [TestCase("cetrizine", "ctz", "antihistamine", "2021-03-15", 0)]
+ [TestCase("cetrizine", "ctz", "antihistamine", "2021-03-15", -55)] 
+ public void PricePerStripTest(string name, string genericMedicineName, string composition, DateTime expiryDate, double pricePerStrip)
+ {
+     Assert.Throws<Exception>(() => program.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, pricePerStrip));
+ }
+
+ [Test]
+ [TestCase("cetrizine", "ctz", "antihistamine", "2020-03-15", 1)]
+ public void ExpireDateExceptionTest(string name, string genericMedicineName, string composition, DateTime expiryDate, double pricePerStrip)
+ {
+     Assert.Throws<Exception>(() => program.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, pricePerStrip));
+ }
+
+ 
+ [Test]
+ [TestCase(1, "2021-03-15", "jahan road")]
+ public void CheckCartoonObjectCreatedTest(int medicineStripCount, DateTime launchDate, string retailerAddress)
+ {
+     var medicine = new Medicine()
+     {
+         Name = "cetrizine",
+         GenericMedicineName = "ctz",
+         Composition = "antihistamine",
+         ExpiryDate = new DateTime(2021, 3, 19),
+         PricePerStrip = 1
+     };
+
+     var result = program.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, medicine);
+
+     Assert.That(result, Is.TypeOf<CartonDetail>());
+ }
+
+ [Test]
+ [TestCase(0, "2021-03-15", "jahan road")]
+ [TestCase(-1, "2021-03-15", "jahan road")]
+ public void MedicineCountTest(int medicineStripCount, DateTime launchDate, string retailerAddress)
+ {
+     var medicine = new Medicine()
+     {
+         Name = "cetrizine",
+         GenericMedicineName = "ctz",
+         Composition = "antihistamine",
+         ExpiryDate = new DateTime(2021, 3, 16),
+         PricePerStrip = 1
+     };
+
+     Assert.Throws<Exception>(() => program.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, medicine));
+ }
+
+ [Test]
+ [TestCase(1, "2020-03-15", "jahan road")]
+ public void IsLaunchDateLessThanCurrentDateTest(int medicineStripCount, DateTime launchDate, string retailerAddress)
+ {
+     var medicine = new Medicine()
+     {
+         Name = "cetrizine",
+         GenericMedicineName = "ctz",
+         Composition = "antihistamine",
+         ExpiryDate = new DateTime(2021, 5, 15),
+         PricePerStrip = 1
+     };
+
+     Assert.Throws<Exception>(() => program.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, medicine));
+ }
+
+ [Test]
+ [TestCase(1, "2021-03-15", "jahan road", null)]
+ public void IsMedicineNullTest(int medicineStripCount, DateTime launchDate, string retailerAddress, Medicine medicine)
+ {
+     var result = program.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, medicine);
+
+     Assert.That(result, Is.EqualTo(null));
+ }
+
+    }
+}
+
+  
